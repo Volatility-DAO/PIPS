6347c510bda9daea274e5b5e7b2a4e8fb8e059a7
Anthony Montalbano <anthony@volatility.com>
2022-02-07 12:59:12 -0500

    generate link to PIP readme on PR

---
+++ branch_pip_path.js
@@ -0,0 +1,44 @@
+// dependencies
+const fse = require('fs-extra')
+
+// constants
+const PROPOSED_PATH = './Proposed';
+const IGNORED_DIRS = ['PIP_TEMPLATE', 'GOVERNANCE_PROPOSAL_TEMPLATE'];
+
+const proposed_paths = [
+	'Adding_An_Index',
+	'Changing_Parameters',
+	'Removing_An_Index',
+	'Volatility_DAO_Governance'
+];
+
+const path_regex = new RegExp('^.\/Proposed\/(' + proposed_paths.join('|') + ')\/(Step_[1-3])\/([a-z0-9\-\_]+)\/README.md', 'i');
+
+async function main() {
+	// detect all status.json files
+	const get_readme_files = function (path, files) {
+		fse.readdirSync(path).forEach((file) => {
+			let subpath = path + '/' + file;
+
+			if (!IGNORED_DIRS.includes(file) && fse.lstatSync(subpath).isDirectory()) {
+				files = get_readme_files(subpath, files);
+			} else {
+
+				let readme_test = subpath.match(path_regex);
+				if (readme_test) {
+					files.push(readme_test);
+				}
+			}
+		});
+
+		return files;
+	}
+
+	// now, let's scan the 3 primary paths
+	const found_readme = get_readme_files(PROPOSED_PATH, []);
+
+	// let's output for use
+	console.log('%s', (found_readme.length > 0) ? found_readme[0][1] + '/' + found_readme[0][2] + '/' + found_readme[0][3] : '');
+}
+
+main();
